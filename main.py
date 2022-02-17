8530a4fdc90c78cdd45878fd65454eee50a2837d
Eoin O'Dowd <46668191+eodowd@users.noreply.github.com>
2022-02-17 00:43:24 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+import xml.etree.ElementTree as ET
+from webui import WebUI
+import os
+from flask import Flask, render_template,flash, request, redirect
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+ui = WebUI(app)
+
+
+UPLOAD_FOLDER = ''
+ALLOWED_EXTENSIONS = {'dspreset','xml'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+header = """
+
+<control>
+default_path= // relative path of your samples
+
+<global>
+// parameters that affect the whole instrument go here.
+
+// *****************************************************************************
+// Your mapping starts here
+// *****************************************************************************
+
+<group> // 1
+
+// Parameters that affect multiple regions go here
+
+
+  trigger=attack    // or release or first or legato
+  loop_mode=no_loop // or loop_continuous or one_shot or loop_sustain
+
+
+"""
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            tree = ET.parse(file.filename)
+            f = open('instrument.sfz', 'w')
+            f.write(header)
+            for neighbor in tree.iter('sample'):
+                getchi = neighbor.get('path')
+                root = neighbor.get('rootNote')
+                lokey = neighbor.get('loNote')
+                hikey = neighbor.get('hiNote')
+                hivel = neighbor.get('hiVel')
+                lovel = neighbor.get('loVel')
+
+                if(hivel == None and lovel == None):
+                    hivel = 127
+                    lovel = 0
+                    f.write('<region>' + 'sample='+str(getchi)+' key= '+str(root)+' lokey= '+str(lokey)+' hikey= '+str(hikey)+' pitch_keycenter= '+str(root)+' hivel= '+str(hivel)+' lovel= '+str(lovel) + '\n')
+                else:
+                    f.write('<region>' + 'sample=' + str(getchi) + ' key= ' + str(root) + ' lokey= ' + str(lokey) + ' hikey= ' + str(hikey) + ' pitch_keycenter= ' + str(root) + ' hivel= ' + str(hivel) + ' lovel= ' + str(lovel) + '\n')
+        f.close()
+        return render_template("info.html")
+
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('index.html'), 404
+
+if __name__ == '__main__':
+    ui.run()
